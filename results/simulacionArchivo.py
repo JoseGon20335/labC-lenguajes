b522638eedc8d5189cf22c5ca7691c218a9649c3
JoseGonzalez098 <gon20335@uvg.edu.gt>
2023-06-05 19:23:38 -0600

    ya

--- results/simulacionArchivo.py
+++ results/simulacionArchivo.py
@@ -1,20 +1,12 @@
 def tokens(listaTokens):
 	for tokenValue in listaTokens:
 		token = tokenValue[1].replace("#","")
-		if token == "ws":
-			return WHITESPACE
-		elif token == "id":
+		if token == "id":
 			return ID
-		elif token == "number":
-			return NUMBER
 		elif token == "+":
 			return PLUS
-		elif token == "-":
-			return MINUS
 		elif token == "*":
 			return TIMES
-		elif token == "/":
-			return DIV
 		elif token == "(":
 			return LPAREN
 		elif token == ")":
